be5fae69cb943f75f138cf917bc28a8365f081ff
Devon Murphy <devonmurphy1@gmail.com>
2016-06-11 06:34:15 +0000

    First commit

---
+++ birdfeederPI3.py
@@ -0,0 +1,15 @@
+from gpiozero import MotionSensor
+from picamera import PiCamera
+from datetime import datetime
+camera = PiCamera()
+pir = MotionSensor(4)
+while True:
+    pir.wait_for_motion()
+    print 'motion detected'
+    filename = datetime.now().strftime("/var/www/html/%Y-%m-%d_%H.%M.%S.h264")
+    camfilename = datetime.now().strftime("/var/www/html/%Y-%m-%d_%H.%M.%S.jpg")
+    camera.capture(camfilename)
+    camera.start_recording(filename)
+    pir.wait_for_no_motion()
+    camera.stop_recording()
+    print 'motion ended'
