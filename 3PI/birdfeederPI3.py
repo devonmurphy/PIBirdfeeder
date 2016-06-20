65ef7f02c79096fc39ac1a9a1a4e8dc44b835203
Devon Murphy <devonmurphy1@gmail.com>
2016-06-20 06:03:29 +0000

    Found a way to just upload to google drive, changed files to make this happen

--- 3PI/birdfeederPI3.py
+++ 3PI/birdfeederPI3.py
@@ -2,16 +2,21 @@ from gpiozero import MotionSensor
 from picamera import PiCamera
 from datetime import datetime
 import time
+import os
 
+motion_timeout = 6;
+next_video_timeout = 0;
 camera = PiCamera()
 pir = MotionSensor(4)
 while True:
     pir.wait_for_motion()
     print 'motion detected'
-    filename = datetime.now().strftime("/var/www/html/%Y-%m-%d_%H.%M.%S.h264")
-    camfilename = datetime.now().strftime("/var/www/html/%Y-%m-%d_%H.%M.%S.jpg")
+    filename = datetime.now().strftime("%Y-%m-%d_%H.%M.%S.h264")
+    camfilename = datetime.now().strftime("%Y-%m-%d_%H.%M.%S.jpg")
     camera.capture(camfilename)
     camera.start_recording(filename)
-    pir.wait_for_no_motion()
+    pir.wait_for_no_motion(motion_timeout)
     camera.stop_recording()
     print 'motion ended'
+    os.system("sudo ./grive -u")
+    time.sleep(next_video_timeout);
